62d6bb9a4e25d5abff83f36814d4a863c12c2479
Mhmd-Hisham <Mohamed00Hisham@Gmail.com>
2024-06-27 04:57:21 +0300

    added implementation benchmark test

--- xor_test.py
+++ xor_test.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import random
+import statistics
 from pprint import pprint
 
 from NEATGenome import NEATGenome
 from InnovationTracker import InnovationTracker
 from NEATPopulation import NEATPopulation
+from NodeGene import Node
+
+# seed the random number generator
+random.seed(12345)
 
 dataset = [({1:0, 2:0}, {3:0}),
            ({1:1, 2:0}, {3:1}),
@@ -31,33 +37,75 @@ def xor_fitness_function(genome, dataset: list) -> float:
 
     return round((4-total_diff)**2, genome.precision)
 
-innv_tracker = InnovationTracker()
-initial_genome = NEATGenome.FullFeedForwardNetwork((2,1),
-                                                    xor_fitness_function,
-                                                    innv_tracker)
+def xor_test(max_generations: int=100, verbose: bool=True):
+    innv_tracker = InnovationTracker()
+    initial_genome = NEATGenome.FullFeedForwardNetwork((2,1),
+                                                        xor_fitness_function,
+                                                        innv_tracker)
 
-population = NEATPopulation(initial_genome)
+    population = NEATPopulation(initial_genome)
 
-accuracy = 0
-max_fitness = 16.0
+    accuracy = 0
+    max_fitness = 16.0
 
-# Evolve while accuracy is less than 95 percent
-while (accuracy < 95.0):
-    population.apply_simulation(dataset)
-    population.evolve()
-    print(f"Generation: {population.current_generation}, " +
-          f"Accuracy={accuracy}%",
-          flush=True)
+    # Evolve while accuracy is less than 95 percent or until max_generations
+    while (accuracy < 95.0 and population.current_generation < max_generations):
+        population.apply_simulation(dataset)
+        population.evolve()
+        if (verbose):
+            print(f"Generation: {population.current_generation}, Accuracy={accuracy}%", flush=True)
 
-    accuracy = round(100*population.top_genome.fitness/max_fitness, 2)
+        accuracy = round(100*population.top_genome.fitness/max_fitness, 2)
 
-pprint(population.top_genome.nodes)
-pprint(population.top_genome.connections)
+    return population.top_genome, population.current_generation
 
-# uncomment if you have graphviz installed [pip install graphviz]
-# you can see a visualization for the network
+def xor_benchmark(n_runs=100):
+    # run the test 100 times to benchmark the algorithm
+    # From the official NEAT paper:
+    # """ 
+    #    On 100 runs, the first experiment shows that the NEAT system finds a structure for
+    #    XOR in an average of 32 generations (4,755 networks evaluated, std 2,553). On average
+    #    a solution network had 2.35 hidden nodes and 7.48 nondisabled connection genes. The
+    #    number of nodes and connections is close to optimal considering that the smallest pos-
+    #    sible network has a single hidden unit (Figure 5(b)). NEAT is very consistent in finding
+    #    a solution. It did not fail once in 100 simulations. The worst performance took 13,459
+    #    evaluations, or about 90 generations (compared to 32 generations on average). The
+    #    standard deviation for number of nodes used in a solution was 1.11, meaning NEAT
+    #    very consistently used 1 or 2 hidden nodes to build an XOR network. In conclusion,
+    #    NEAT solves the XOR problem without trouble and in doing so keeps the topology
+    #    small.
+    # """
+    n_hidden_nodes = []
+    n_generations = []
+    n_connections = []
 
-# from NNVisualizer import NNVisualizer
-# NNVisualizer(population.top_genome, "Top Genome").view()
+    for _ in range(n_runs):
+        genome, generation = xor_test(max_generations=100, verbose=False)
+        hidden_nodes = [node for node in genome.nodes.values() if node.type == Node.Hidden]
+        nondisabled_connections = [conn for conn in genome.connections.values() if not conn.disabled]
+        n_hidden_nodes.append(len(hidden_nodes))
+        n_generations.append(generation)
+        n_connections.append(len(nondisabled_connections))
 
-#
+    print(f"Average number of hidden nodes: {sum(n_hidden_nodes)/n_runs}")
+    print(f"Standard deviation: {statistics.stdev(n_hidden_nodes)}")
+    print()
+    print(f"Average number of generations: {sum(n_generations)/n_runs}")
+    print(f"Standard deviation: {statistics.stdev(n_generations)}")
+    print()
+    print(f"Average number of connections: {sum(n_connections)/n_runs}")
+    print(f"Standard deviation: {statistics.stdev(n_connections)}")
+    print()
+    print(f"Optimal solution found in {n_hidden_nodes.count(1)} of {n_runs} runs")
+
+    return genome
+
+if __name__ == "__main__":
+    top_genome = xor_benchmark()
+    
+    # uncomment if you have graphviz installed [pip install graphviz]
+    # you can see a visualization for the network
+
+    # from NNVisualizer import NNVisualizer
+    # NNVisualizer(top_genome, "Top Genome").view()
+    
